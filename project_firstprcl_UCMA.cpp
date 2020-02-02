a66ee19c2b2881d65143a4faf748b1992561628d
RicardoCamposV <ricard_halo@live.com.mx>
2020-02-02 05:49:55 -0600

    Add first project UCMA

--- project_firstprcl_UCMA.cpp
+++ project_firstprcl_UCMA.cpp
@@ -20,35 +20,77 @@ int show_menu(){
 }
 
 
+int combat(float h_enemy, float a_enemy, float h_pikachu, const string& my_pokemon, string& name_pokemon){
+    int total = 0;
+    char user_select_damage;
+
+    cout<<"Tu oponente sera "<<name_pokemon <<endl;
+    cout<<""<<my_pokemon <<" vs "<<name_pokemon <<endl;
+    cout<<"¡Que empiece la pelea!\n\n";
+
+    while(h_pikachu > 0 and h_enemy > 0){
+
+        cout<<"\n(a)Ataque chispazo / (b)Ataque bola voltio\n";
+        cin>>user_select_damage;
+
+        if(user_select_damage == 'a'){
+            h_enemy -= 9.5;
+            cout<<""<<my_pokemon <<" hizo un ataque de 9.5 de daño\n";
+        }else if(user_select_damage == 'b'){
+            h_enemy -= 12.5;
+            cout<<""<<my_pokemon <<" hizo un ataque de 12.5 de daño\n";
+        }
+
+        cout<<"La vida de "<<name_pokemon <<" es de: "<<h_enemy<<endl;
+        cout<<""<<name_pokemon <<" te hizo un ataque de "<<a_enemy <<" de daño"<<endl;
+
+        h_pikachu -= a_enemy;
+        cout<<"La vida de "<<my_pokemon <<": "<<h_pikachu<<endl;
+
+    }
+
+    if(h_pikachu <= 0){
+        cout<<"\n¡"<<my_pokemon <<" a perdido!\n";
+    }
+    if(h_enemy <= 0){
+        cout<<"\n¡"<<name_pokemon <<" a perdido!\n";
+    }
+
+    cout<<"El combate ha terminado\n";
+    return(total);
+}
+
+
 //Ricardo Campos Villasana - Programación 2
 int main() {
-    int health_pikachu = 100, final_decision;
-    string option_user;
+    int final_decision;
+    float health_pikachu = 100;
+    string option_user, my_pokemon = "Pikachu";
 
     while(option_user != "n"){
         final_decision = show_menu();
 
         if(final_decision == BULBASSAUR){
-            int health_enemy = 100;
+            //int health_enemy = 100;
             string name_pokemon = "Bulbasaur";
-            float attack_pokemon = 10.5;
+            float attack_pokemon = 10.5, health_enemy = 100;
 
-            cout<<health_enemy<<endl;
-            cout<<attack_pokemon<<endl;
+            combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
+
         }else if(final_decision == CHARMANDER){
-            int health_enemy = 80;
+            //int health_enemy = 80;
             string name_pokemon = "Charmander";
-            float attack_pokemon = 7.5;
+            float attack_pokemon = 7.5, health_enemy = 80;
 
-            cout<<health_enemy<<endl;
-            cout<<attack_pokemon<<endl;
+            combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
+
         }else if(final_decision == SQUIRTL){
-            int health_enemy = 90;
+            //int health_enemy = 90;
             string name_pokemon = "Squirtl";
-            float attack_pokemon = 8.5;
+            float attack_pokemon = 8.5, health_enemy = 90;
 
-            cout<<health_enemy<<endl;
-            cout<<attack_pokemon<<endl;
+            combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
+
         }
         cout<<"Desea jugar?: Si (s) / No (n)\n";
         cin>>option_user;
