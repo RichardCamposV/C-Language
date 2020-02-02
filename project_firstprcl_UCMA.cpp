33c3f8e120c70bb111497e646bd1a0ad3422100b
RicardoCamposV <ricard_halo@live.com.mx>
2020-02-02 16:19:16 -0600

    Add first project UCMA

--- project_firstprcl_UCMA.cpp
+++ project_firstprcl_UCMA.cpp
@@ -24,7 +24,7 @@ void combat(float h_enemy, float a_enemy, float h_pikachu, const string& my_poke
     float attack_spark = 9.5, attack_ballvolt = 12.5;
     char user_select_damage;
 
-    cout<<"Tu oponente sera "<<name_pokemon <<endl;
+    cout<<"\nTu oponente sera "<<name_pokemon <<endl;
     cout<<""<<my_pokemon <<" vs "<<name_pokemon <<endl;
     cout<<"¡Que empiece la pelea!\n\n";
 
@@ -60,30 +60,64 @@ void combat(float h_enemy, float a_enemy, float h_pikachu, const string& my_poke
 }
 
 
+float difficulty(){
+    int user_difficulty;
+    float difficulty = 0;
+
+    cout<<"\n----- Elige la dificultad -----\n";
+    cout<<"-------------------------------\n";
+    cout<<"-----   1) Por defecto    -----\n";
+    cout<<"-----     2) Dificil      -----\n";
+    cout<<"-------------------------------\n\n";
+    cout<<"Elige la dificultad deseada: ";
+    cin>>user_difficulty;
+
+    switch (user_difficulty){
+        case 1:
+            return(difficulty);
+        case 2:
+            difficulty += 5;
+            return(difficulty);
+        default:
+            cout<<"¡Opcion invalida!\n";
+    }
+    return 0;
+}
+
+
 //Ricardo Campos Villasana - Programación 2
 int main() {
     int final_decision;
-    float health_pikachu = 100;
+    float health_pikachu = 100, final_difficulty;
     string option_user, my_pokemon = "Pikachu";
 
     while(option_user != "n"){
         final_decision = show_menu();
 
         if(final_decision == BULBASSAUR){
+            final_difficulty = difficulty();
             string name_pokemon = "Bulbasaur";
             float attack_pokemon = 11.5, health_enemy = 100;
+
+            attack_pokemon = attack_pokemon + final_difficulty;
 
             combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
 
         }else if(final_decision == CHARMANDER){
+            final_difficulty = difficulty();
             string name_pokemon = "Charmander";
             float attack_pokemon = 8.5, health_enemy = 80;
+
+            attack_pokemon = attack_pokemon + final_difficulty;
 
             combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
 
         }else if(final_decision == SQUIRTL){
+            final_difficulty = difficulty();
             string name_pokemon = "Squirtl";
             float attack_pokemon = 10.5, health_enemy = 90;
+
+            attack_pokemon = attack_pokemon + final_difficulty;
 
             combat(health_enemy, attack_pokemon, health_pikachu, my_pokemon, name_pokemon);
 
